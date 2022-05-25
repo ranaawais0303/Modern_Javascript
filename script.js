eec67db133d4beb02958ff0a403ce8f3c45326e1
Rana_awais <z.manzoor@pioneerlogics.com>
2022-05-25 06:10:47 -0700

    module 1

---
+++ script.js
@@ -0,0 +1,5 @@
+//importing module
+import './shoppingCart.js';
+// import { addToCart } from './shoppingCart.js';
+console.log('importing the module');
+// addToCart('my', 40);
---
+++ shoppingCart.js
@@ -0,0 +1,12 @@
+//Exporting the module
+console.log('Exporting the module');
+
+// const cart = [];
+
+// // const addToCart=function()
+// const addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart`);
+// };
+
+// export { addToCart };
